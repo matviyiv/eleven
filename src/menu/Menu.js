0267e8b6587eb91402d87bfc20935ad8b9ad0a30
Natalya Fitak <natalyafitak@Natalyas-MacBook-Air.local>
2017-04-02 21:12:30 +0400

    added fonts  and temporary put all css in menu

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -5,24 +5,36 @@ import { Link } from 'react-router-dom';
 
 export default class Menu extends Component {
   render() {
-    return (<header className="menu">
-      <hgroup className="menu--hgroup">
-        <h1>
-          <Link to="/home" className="menu--logo"><image src={logo}/></Link>
-        </h1>
-        <nav className="nav navy menu--nav">
-          <div className="menu--button"><i className="fa fa-bars" aria-hidden="true"></i>Navigation</div>
-          <ul className="nav nav-tabs menu--tabs">
-            <li className="active"><Link to="/home" title="Головна">Головна</Link></li>
-            <li><Link to="/main" title="Про нас">Про нас</Link></li>
-            <li><Link to="/main" title="Підписатись">Підписатись</Link></li>
-            <li><Link to="/main" title="Контакти">Контакти</Link></li>
-          </ul>
-        </nav>
-      </hgroup>
-      <footer className="desk menu--footer">
-        <p>© All rights reserved. Eleven 2017 Made with <i class="fa fa-heart" aria-hidden="true"></i></p>
-      </footer>
-    </header>)
+    return (
+<header role="header">
+  <hgroup> 
+    <h1> <a href="#" title="eleven">
+      <Link to="/home" className="menu--logo"><image src={logo}/></Link>
+    </a> </h1>
+    <nav role="nav" id="header-nav" className="nav navy">
+      <ul className="nav nav-tabs">
+        <li className="active"><a data-toggle="tab" href="#home" title="Головна">Головна</a></li>
+        <li><a data-toggle="tab" href="#menu1" title="Про нас">Про нас</a></li>
+        <li><a data-toggle="tab" href="#menu2" title="Підписатись">Підписатись</a></li>
+        <li><a data-toggle="tab" href="#menu3" title="Контакти">Контакти</a></li>
+      </ul>
+      <div role="socil-icons" className="mobile-social">
+        <li><a href="#" target="_blank" title="twitter"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+        <li><a href="#" target="_blank" title="facebook"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+        <li><a href="#" target="_blank" title="google-plus"><i className="fa fa-google-plus" aria-hidden="true"></i></a></li>
+        <li><a href="#" target="_blank" title="pinterest"><i className="fa fa-pinterest" aria-hidden="true"></i></a></li>
+      </div>
+    </nav>
+    <ul role="socil-icons" className="desk-social">
+      <li><a href="#" target="_blank" title="twitter"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+      <li><a href="#" target="_blank" title="facebook"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+      <li><a href="#" target="_blank" title="google-plus"><i className="fa fa-google-plus" aria-hidden="true"></i></a></li>
+      <li><a href="#" target="_blank" title="pinterest"><i className="fa fa-pinterest" aria-hidden="true"></i></a></li>
+    </ul>
+  </hgroup>
+  <footer className="desk">
+     <p>&copy; All rights reserved. Eleven 2017 Made with <i className="fa fa-heart" aria-hidden="true"></i></p>
+  </footer>
+</header>)
   }
 }
