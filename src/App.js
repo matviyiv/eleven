6ab6fcb1b63dc31e7b346a6b31b891fba121b705
Nazar Matviyiv <matviyiv.nazar@gmail.com>
2017-06-09 13:16:04 +0300

    Add to homescreen anabled

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 
+import {addToHomescreen} from './components/add-to-home-screen';
+
 import BackgroundSlider from './components/background-slider/BackgroundSlider';
 import Menu from './components/menu/Menu';
 
 export default class App extends Component {
+  componentDidMount() {
+    addToHomescreen.language = 'uk_ua';
+    const addtohome = addToHomescreen({
+      autostart: false,
+      skipFirstVisit: true,
+      maxDisplayCount: 1,
+      lifespan: 30,
+    });
+
+    addtohome.show();
+  }
   render() {
     return (
       <div className="app">
