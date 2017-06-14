bb011ad5984062db88fdb181e38aadb7bebb7f12
Nazar Matviyiv <matviyiv.nazar@gmail.com>
2017-06-14 12:38:38 +0300

    Added more tests

---
+++ src/flux/__tests__/reducer.test.js
@@ -0,0 +1,43 @@
+import moment from 'moment';
+import _ from 'lodash';
+
+import * as reducers from '../reducers';
+import * as actions from '../actions';
+
+describe('reducers', () => {
+  let st;
+
+  beforeEach(() => {
+    st = {
+      booking: {
+        selectedServices: {}
+      },
+      services: {
+        id: 's2',
+        list: [{
+          sub: [{
+            id: 's2_1',
+            duration: 1,
+            name: 'service2'
+          }]
+        }]
+      }
+    }
+  })
+  it('SELECT_MASTER_NEXT_DATE', () => {
+    const data = {
+      serviceId: 's2_1',
+      masterId: 'e1',
+      date: moment(new Date('Wed Jun 14 2017 11:00:00 GMT+0300 (EEST)')).hours(11).minutes(0).seconds(0)
+    };
+    const result = reducers.appReducer(st, actions.selectMasterNextDate(data));
+    const expected = {
+      dateEnd: "Wed Jun 14 2017 14:30:00 GMT+0300 (EEST)",
+      dateStart: "Wed Jun 14 2017 13:30:00 GMT+0300 (EEST)",
+      duration: 1,
+      masterId: "e1",
+      name: "вечірній"
+    };
+    expect(result.booking.selectedServices).toMatchSnapshot();
+  });
+});
